990abab6554898b1b4e50a98c56baaa342e04369
cjk <2217163259@qq.com>
2021-10-16 13:23:05 +0800

    first commit

---
+++ src/main/java/com/chen/git/Test.java
@@ -0,0 +1,13 @@
+package com.chen.git;
+
+/**
+ * @author chenjunkang
+ * @date 2021/10/16
+ * @apiNote
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
