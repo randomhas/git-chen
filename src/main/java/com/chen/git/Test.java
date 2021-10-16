98fdd3faa8e017711b1d7920347860cf2b1a14dd
cjk <2217163259@qq.com>
2021-10-16 13:24:44 +0800

    second commit

--- src/main/java/com/chen/git/Test.java
+++ src/main/java/com/chen/git/Test.java
@@ -8,6 +8,7 @@ package com.chen.git;
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("hello git");
+        System.out.println("hello git1");
+        System.out.println("hello git2");
     }
 }
